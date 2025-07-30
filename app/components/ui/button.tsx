3461b7e5490639ae9d4d240a990773619eb9cce4
Felipe Dornelles <lipe.dornelles@hotmail.com>
2025-07-30 20:57:29 -0300

    feat: button component

---
+++ app/components/ui/button.tsx
@@ -0,0 +1,23 @@
+import { cn } from "@/app/lib/utils";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant?: "primary" | "secundary" | "ghost";
+}
+
+export default function Button({ children, variant, ...props }: ButtonProps) {
+  return (
+    <button
+      {...props}
+      className={cn(
+        "p-3 text-white rounded-xl font-bold whitespace-nowrap hover:opacity-95 disabled:opacity-70",
+        variant === "primary" && "bg-accent-purple",
+        variant === "secundary" && "bg-background-tertiary",
+        variant === "ghost" && "border-border-primary",
+        props.className
+      )}
+    >
+      {children}
+    </button>
+  );
+}
