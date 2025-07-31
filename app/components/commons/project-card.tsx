a7c9dd7512a3cb32610faf79ba6bf1c1e77df4d2
Felipe Dornelles <lipe.dornelles@hotmail.com>
2025-07-31 09:11:51 -0300

    feat: project card component

---
+++ app/components/commons/project-card.tsx
@@ -0,0 +1,18 @@
+export default function ProjectCard() {
+  return (
+    <div className="w-[430px] h-[132px] flex gap-5 bg-background-secondary p-3 rounded-[20px] border border-transparent hover:border-border-secondary">
+      <div className="size-24 rounded-md overflow-hidden flex-shrink-0">
+        <img src="/project1.jpg" alt="" className="w-full h-full object-cover" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <span className="uppercase text-xs font-bold text-accent-green">10 cliques</span>
+        <div className="fkex flex-col">
+          <span className="text-white font-bold">Projeto 1</span>
+          <span className="text-content-body text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
