ca1ecc29a1a51800c33b539d03d543910438725e
Felipe Dornelles <lipe.dornelles@hotmail.com>
2025-12-22 17:03:31 -0300

    feat: add SEO metadata handling with getSEOTags function

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,8 +1,14 @@
+import { Metadata } from "next";
 import FAQ from "../components/landing-page/faq";
 import Header from "../components/landing-page/header";
 import Hero from "../components/landing-page/hero";
 import Pricing from "../components/landing-page/pricing";
 import VideoExplanation from "../components/landing-page/video-explanation";
+
+export const metadata: Metadata = {
+  title: "ProjectInBio",
+  description: "ProjectInBio -  A plataforma de gestão de projetos",
+};
 
 export default function Home() {
   return (
